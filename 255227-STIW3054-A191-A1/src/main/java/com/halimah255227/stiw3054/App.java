2cdcb952482fcc4c9900f90c6c5e08dcf8162bdf
SilentHLive <halimahramli6596@gmail.com>
2019-10-08 11:10:23 +0800

    Initial commit

---
+++ 255227-STIW3054-A191-A1/src/main/java/com/halimah255227/stiw3054/App.java
@@ -0,0 +1,13 @@
+package com.halimah255227.stiw3054;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
